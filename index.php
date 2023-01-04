a8b6c7750bf8dbcd1b0b3b2b07b3801a49c2ffef
Alex A. Cita <acita@uolinc.com>
2023-01-04 15:27:04 -0300

    implementando rotas CRUD

--- index.php
+++ index.php
@@ -55,14 +55,24 @@ $app->get('/admin/users/create', function () {
 
 $app->get('/admin/users/:iduser/delete', function ($iduser) {
     User::verifyLogin();
+
+    $user = new User();
+    $user->get((int)$iduser);
+    $user->delete();
+    header('Location: /admin/users');
     exit;
-    header('Location: /users');
 });
 
 $app->get('/admin/users/:iduser', function ($iduser) {
     User::verifyLogin();
+    $user = new User();
+    $user->get((int)$iduser);
+    
     $page = new AdminController();
-    $page->setTpl('users-update');
+
+    $page->setTpl('users-update', array(
+        "user"=>$user->getValues()
+    ));
     exit;
 });
 
@@ -78,13 +88,25 @@ $app->get('/admin/users', function () {
 
 $app->post('/admin/users/create', function () {
     User::verifyLogin();
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->setData($_POST);
+    $user->save();
+    header('Location: /admin/users');
     exit;
 });
 
-$app->post('/admin/update/:iduser', function ($iduser) {
+$app->post('/admin/user/update/:iduser', function ($iduser) {
     User::verifyLogin();
+
+    $user = new User();
+    $user->get((int)$iduser);
+    $user->setData($_POST);
+    $user->update();
+    header('Location: /admin/users');
     exit;
-    header('Location: /users');
 });
 
 $app->run();
--- src/Model/User.php
+++ src/Model/User.php
@@ -4,6 +4,7 @@ namespace Model;
 
 use BD\ConnectSQL;
 use Model\Model;
+use PSpell\Config;
 
 class User extends Model {
 
@@ -73,8 +74,66 @@ class User extends Model {
         return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
     }
 
-    public static function saveData()
+    public function save()
     {
-        
+        $sql = new ConnectSQL();
+
+        $results = $sql->select("CALL sp_users_save (
+            :desperson, 
+            :deslogin, 
+            :despassword, 
+            :desemail, 
+            :nrphone, 
+            :inadmin)", array(
+            ":desperson"=>$this->getdesperson(),
+            ":deslogin"=>$this->getdeslogin(),
+            ":despassword"=>$this->getdespassword(),
+            ":desemail"=>$this->getdesemail(),
+            ":nrphone"=>$this->getnrphone(),
+            ":inadmin"=>$this->getinadmin()
+            ));
+
+        $this->setData($results[0]);
+    }
+
+    public function get($iduser){
+        $sql = new ConnectSQL();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) WHERE a.iduser = :iduser", array(
+            ":iduser"=>$iduser
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function update(){
+
+        $sql = new ConnectSQL();
+
+        $results = $sql->select("CALL sp_usersupdate_save (
+            :iduser,
+            :desperson, 
+            :deslogin, 
+            :despassword, 
+            :desemail, 
+            :nrphone, 
+            :inadmin)", array(
+                ":iduser"=>$this->getiduser(),
+                ":desperson"=>$this->getdesperson(),
+                ":deslogin"=>$this->getdeslogin(),
+                ":despassword"=>$this->getdespassword(),
+                ":desemail"=>$this->getdesemail(),
+                ":nrphone"=>$this->getnrphone(),
+                ":inadmin"=>$this->getinadmin()
+            ));
+
+        $this->setData($results[0]);
+    }
+
+    public function delete(){
+        $sql = new ConnectSQL();
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser"=>$this->getiduser()
+        ));
     }
 }
